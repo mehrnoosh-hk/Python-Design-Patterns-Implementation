9724f2aa1c739f4089d3e7ef388342ba219932a6
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2023-03-25 23:20:28 +0330

    Refactor to be more efficient

--- creational/thread_safe_singleton.py
+++ creational/thread_safe_singleton.py
@@ -4,14 +4,15 @@ from typing import Any
 
 class ThreadSafeSingletonMeta(type):
     _instances = {}
+    _lock = threading.Lock()
 
     def __call__(cls, *args: Any, **kwds: Any) -> Any:
-        with threading.Lock():
+        with cls._lock:
             if cls not in cls._instances:
                 cls._instances[cls] = super().__call__(*args, **kwds)
         return cls._instances[cls]
 
 
 class ThreadSafeSingleton(metaclass=ThreadSafeSingletonMeta):
-    def some_logic():
+    def some_logic(self):
         pass
